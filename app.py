5b77b08db738cc2ef7e8b7060bc9c7c1f9f616bc
KARNN-SINGH <karnngurjar968@gmail.com>
2026-06-02 14:01:21 +0530

    Update app.py

--- app.py
+++ app.py
@@ -34,9 +34,15 @@ st.set_page_config(
     page_icon="🎵",
     layout="wide"
 )
-col1, col2, col3 = st.columns([1,2,1])
+st.markdown(
+    """
+    <div style="text-align: center;">
+        <img src="logo.png" width="250">
+    </div>
+    """,
+    unsafe_allow_html=True
+)
 
-with col2:
     st.image("logo.png", width=250)
 st.title("🎵 7BROTHER'S INDIAN AI CREATOR")
 st.markdown("""
