c7b06a25e9a8660311d0ffa7fe1e06ad891a2293
KARNN-SINGH <karnngurjar968@gmail.com>
2026-06-02 17:33:56 +0530

    Update app.py

--- app.py
+++ app.py
@@ -121,8 +121,8 @@ if st.button("🚀 Generate Song"):
     tera gun gaan sab gaaye।
     """
 
-st.write(lyrics)
-st.download_button(
+    st.write(lyrics)
+    st.download_button(
         label="⬇️ Download Lyrics",
         data=lyrics,
         file_name="7brothers_song.txt",
