aac928842a0f967a1f266be516df05892157ebea
KARNN-SINGH <karnngurjar968@gmail.com>
2026-06-02 20:08:12 +0530

    Update app.py

--- app.py
+++ app.py
@@ -133,12 +133,12 @@ if st.button("🚀 Generate Song"):
     st.markdown(f"## {title}")
     st.write(lyrics)
 
-st.download_button(
-    label="⬇️ Download Lyrics",
-    data=lyrics,
-    file_name="7brothers_song.txt",
-    mime="text/plain"
-)
+    st.download_button(
+        label="⬇️ Download Lyrics",
+        data=lyrics,
+        file_name="7brothers_song.txt",
+        mime="text/plain"
+    )
 
 
 st.divider()
