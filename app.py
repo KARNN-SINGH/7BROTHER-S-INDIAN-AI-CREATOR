b5f94bd2cfcb9365f912bf5d58178bb439592bee
KARNN-SINGH <karnngurjar968@gmail.com>
2026-06-02 20:59:08 +0530

    Update app.py

--- app.py
+++ app.py
@@ -33,7 +33,7 @@ h1 {
 """, unsafe_allow_html=True)
 genai.configure(api_key=st.secrets["GEMINI_API_KEY"])
 
-model = genai.GenerativeModel("gemini-1.5-flash")
+model = genai.GenerativeModel("models/gemini-1.5-flash")
 
 st.write("API key loaded:", "GEMINI_API_KEY" in st.secrets)
 
